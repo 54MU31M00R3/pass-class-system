7199e33a9a655355f2803ed334ba652afa03593b
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-23 20:25:40 -0500

    Add placeholder dashboard

--- frontend/src/shared/components/Navbar.jsx
+++ frontend/src/shared/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div>Hello World!</div>
+    <div className='header'>
+        <ul>
+            <li>Home</li>
+            <li>Dashboard</li>
+            <li>Login</li>
+        </ul>
+    </div>
   )
-}
+};
 
-export default Navbar
+export default Navbar;
