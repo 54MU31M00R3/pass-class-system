1224dc76c0f2b30288aff71f20f6f22dfdcb0995
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-24 18:51:21 -0500

    Add simple CSS to center and align form content

--- frontend/src/users/components/LoginForm.jsx
+++ frontend/src/users/components/LoginForm.jsx
@@ -7,14 +7,18 @@ function LoginForm({ formToggler }) {
 
     return (
         <>
-            <form className='loginForm'>
-                <label htmlFor='email'>Email</label>
-                <input id='email' type='text' />
-                <label htmlFor='password'>Password</label>
-                <input id='password' type='text' />
-                <button onClick={loginSubmit}>LOGIN</button>
-            </form>
-            <button onClick={formToggler}>SWITCH TO SIGNUP</button>
+            <div className='formContainer'>
+                <div className='authForm'>
+                    <form className='formDetails'>
+                        <label htmlFor='email'>Email</label>
+                        <input id='email' type='text' />
+                        <label htmlFor='password'>Password</label>
+                        <input id='password' type='text' />
+                        <button onClick={loginSubmit}>LOGIN</button>
+                    </form>
+                    <button onClick={formToggler}>SWITCH TO SIGNUP</button>
+                </div>
+            </div>
         </>
     )
 }
--- frontend/src/users/components/RegisterForm.jsx
+++ frontend/src/users/components/RegisterForm.jsx
@@ -7,18 +7,22 @@ function RegisterForm({ formToggler }) {
 
     return (
         <>
-            <form className='registerForm'>
-                <label htmlFor='username'>Username</label>
-                <input id='username' type='text' />
-                <label htmlFor='email'>Email</label>
-                <input id='email' type='text' />
-                <label htmlFor='studentNum'>Student Number</label>
-                <input id='studentNum' type='text' />
-                <label htmlFor='password'>Password</label>
-                <input id='password' type='text' />
-                <button onClick={registerSubmit}>SIGNUP</button>
-            </form>
-            <button onClick={formToggler}>SWITCH TO LOGIN</button>
+            <div className='formContainer'>
+                <div className='authForm'>
+                    <form className='formDetails'>
+                        <label htmlFor='username'>Username</label>
+                        <input id='username' type='text' />
+                        <label htmlFor='email'>Email</label>
+                        <input id='email' type='text' />
+                        <label htmlFor='studentNum'>Student Number</label>
+                        <input id='studentNum' type='text' />
+                        <label htmlFor='password'>Password</label>
+                        <input id='password' type='text' />
+                        <button onClick={registerSubmit}>SIGNUP</button>
+                    </form>
+                    <button onClick={formToggler}>SWITCH TO LOGIN</button>
+                </div>
+            </div>
         </>
     )
 }
