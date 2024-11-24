544540d8d460559ef316325f5edf6aba109901ff
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-23 20:35:45 -0500

    Add NavLink component to access other routes.
    Also made links' text color black.

--- frontend/src/shared/components/Navbar.jsx
+++ frontend/src/shared/components/Navbar.jsx
@@ -4,15 +4,23 @@ import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
-  return (
-    <div className='header'>
-        <ul>
-            <li>Home</li>
-            <li>Dashboard</li>
-            <li>Login</li>
-        </ul>
-    </div>
-  )
+    return (
+        <>
+            <div className='header'>
+                <ul>
+                    <li>
+                        <NavLink className='headerLink' to='/'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className='headerLink' to='/:uid/dashboard'>Dashboard</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className='headerLink' to='/login'>Login</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
 };
 
 export default Navbar;
