540c846ff9532c6980cfce65b258a696958ab9d2
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-25 15:41:46 -0500

    Add component to hold form that creates sections

--- frontend/src/content/components/SectionForm.jsx
+++ frontend/src/content/components/SectionForm.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
 function SectionForm() {
-  return (
-    <div>SectionForm</div>
-  )
+
+    const createSectionHandler = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <>
+            <div className='formContainer'>
+                <div className='secForm'>
+                    <form className='formDetails'>
+                        <label htmlFor='courseName'>Course Name</label>
+                        <input id='courseName' type='text' />
+                        <label htmlFor='courseSection'>Course Section</label>
+                        <input id='courseSection' type='text' />
+                        <label htmlFor='timeOfSession'>Time of Session</label>
+                        <input id='timeOfSession' type='text' />
+                        <label htmlFor='buildingRoomNumber'>Location</label>
+                        <input id='buildingRoomNumber' type='text' />
+                        <button onClick={createSectionHandler}>CREATE SECTION</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default SectionForm
