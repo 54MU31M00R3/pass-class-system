a0ad0d2ffbc2699ba2dd028f6a5e667a498a8b79
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-24 23:29:10 -0500

    Create and add section content component
    This component will be used to display the content
    of each sections being announcements or
    worksheets.

---
+++ frontend/src/users/shared/components/SectionContent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SectionContent() {
+  return (
+    <div>SectionContent</div>
+  )
+}
+
+export default SectionContent
--- frontend/src/users/shared/pages/SectionPage.jsx
+++ frontend/src/users/shared/pages/SectionPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import SectionItem from '../components/SectionItem';
+import SectionContent from '../components/SectionContent';
 import sections from '../../../assets/dummyData/sections.json';
 
 function Section() {
@@ -10,12 +11,15 @@ function Section() {
     const section = sections.find(section => section.sectionId == sectionId);
 
     return (
-        <SectionItem
-            courseName={section.courseName}
-            courseSection={section.courseSection}
-            timeOfSession={section.timeOfSession}
-            buildingRoomNumber={section.buildingRoomNumber}
-        />
+        <>
+            <SectionItem
+                courseName={section.courseName}
+                courseSection={section.courseSection}
+                timeOfSession={section.timeOfSession}
+                buildingRoomNumber={section.buildingRoomNumber}
+            />
+            <SectionContent />
+        </>
     )
 }
 
