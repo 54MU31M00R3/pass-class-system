695807d4eedb0dd4502ffa5e679ae2983e345f3d
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-26 18:43:07 -0500

    Add announcement model schema using mongoose
    Also fixed naming typo in worksheet.js

--- backend/models/announcement.js
+++ backend/models/announcement.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const worksheetSchema = new Schema({
+const announcementSchema = new Schema({
     content: { type: mongoose.Types.ObjectId, required: true, ref: 'Content' },    title: { type: String, required: true },
-    filePath: { type: String, required: true }
+    body: { type: String, required: true }
 })
 
-export default mongoose.model('Worksheet', worksheetSchema);
+export default mongoose.model('Announcement', announcementSchema);
