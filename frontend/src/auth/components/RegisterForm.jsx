15c6f9aef2e622e732c028b914274fa630879c9e
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-25 21:34:01 -0500

    Add dummy data for faculty and dummy register func

--- frontend/src/auth/components/RegisterForm.jsx
+++ frontend/src/auth/components/RegisterForm.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from 'react';
+
+import users from '../../assets/dummyData/users.json';
+import faculty from '../../assets/dummyData/faculty.json';
 
 function RegisterForm({ formToggler }) {
     const registerSubmit = (event) => {
         event.preventDefault();
+
+        const facultyMember = faculty.find(employee => employee.id === idNum)
+        let userRole;
+        if (facultyMember) {
+            userRole = facultyMember.role
+        } else {
+            userRole = 'student'
+        }
+
+        const newUser = {
+            username: username.value,
+            email: email.value,
+            id: idNum.value,
+            password: password.value,
+            sectionIds: [],
+            role: userRole 
+        }
+
+        users.push(newUser);
+        console.log(newUser);
+        console.log(users);
+
     }
 
     return (
@@ -13,9 +38,9 @@ function RegisterForm({ formToggler }) {
                         <label htmlFor='username'>Username</label>
                         <input id='username' type='text' />
                         <label htmlFor='email'>Email</label>
-                        <input id='email' type='text' />
-                        <label htmlFor='studentNum'>Student Number</label>
-                        <input id='studentNum' type='text' />
+                        <input id='email' type='email' />
+                        <label htmlFor='idNum'>ID Number</label>
+                        <input id='idNum' type='password' />
                         <label htmlFor='password'>Password</label>
                         <input id='password' type='text' />
                         <button onClick={registerSubmit}>SIGNUP</button>
