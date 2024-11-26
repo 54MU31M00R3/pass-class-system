32c45364ba65408f4658fc54b9f18fb0bffe9a60
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-25 20:51:28 -0500

    Add react createContext component to track auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import {
   Route,
   createBrowserRouter,
@@ -13,24 +13,74 @@ import Dashboard from './shared/pages/Dashboard';
 import SectionOverview from './content/pages/SectionOverview';
 import CreateSection from './content/pages/CreateSection';
 import UploadContent from './content/pages/UploadContent';
-
+import { AuthContext } from './shared/context/auth-context';
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<MainLayout />} >
-        <Route index element={<HomePage />} />
-        <Route path='/login' element={<Authentication />} />
-        <Route path='/:userId/dashboard' element={<Dashboard />} />
-        <Route path='/section/create' element={<CreateSection />} />
-        <Route path='/:sectionId/section' element={<SectionOverview />} />
-        <Route path='/:sectionId/section/content/upload' element={<UploadContent />} />
-        <Route path='*' />
-      </Route>
-    )
-  )
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState();
+  const [role, setRole] = useState();
 
-  return <RouterProvider router={router} />
+  const login = useCallback((userId, userRole) => {
+    setIsLoggedIn(true);
+    setUserId(userId);
+    setRole(userRole)
+  }, [])
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+    setUserId(null);
+    setRole(null)
+  })
+
+  let router;
+
+  if (isLoggedIn) {
+    router = createBrowserRouter(
+      createRoutesFromElements(
+        <Route path='/' element={<MainLayout />} >
+          <Route index element={<HomePage />} />
+          <Route path='/login' element={<Authentication />} />
+          <Route path='*' element={<HomePage />} />
+        </Route>
+      )
+    )
+  } else if (isLoggedIn && (role === 'student')) {
+    router = createBrowserRouter(
+      createRoutesFromElements(
+        <Route path='/' element={<MainLayout />} >
+          <Route index element={<HomePage />} />
+          <Route path='/:userId/dashboard' element={<Dashboard />} />
+          <Route path='/:sectionId/section' element={<SectionOverview />} />
+          <Route path='*' />
+        </Route>
+      )
+    )
+  } else if (isLoggedIn && (role === 'pass leader')) {
+    router = createBrowserRouter(
+      createRoutesFromElements(
+        <Route path='/' element={<MainLayout />} >
+          <Route index element={<HomePage />} />
+          <Route path='/:userId/dashboard' element={<Dashboard />} />
+          <Route path='/section/create' element={<CreateSection />} />
+          <Route path='/:sectionId/section' element={<SectionOverview />} />
+          <Route path='/:sectionId/section/content/upload' element={<UploadContent />} />
+          <Route path='*' />
+        </Route>
+      )
+    )
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn,
+      userId: userId,
+      role: userRole,
+      login: login,
+      logout: logout
+    }}>
+      <RouterProvider router={router} />
+    </AuthContext.Provider>
+  )
 }
 
 export default App;
