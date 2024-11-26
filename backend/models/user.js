a1943d1f93322b57ae2a936eacc5109da4b2f2b1
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-26 17:32:45 -0500

    Create user model schema using mongoose

---
+++ backend/models/user.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 5 },
+    role: { type: String, required: true },
+    yuId: { type: String, required: true, minlength: 9, unique: true},
+    sections: [{type: mongoose.Types.ObjectId, required: true, ref: 'Section'}]
+})
+
+export default mongoose.model('User', userSchema);
