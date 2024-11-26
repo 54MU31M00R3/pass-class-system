07f01606f3747f7e85c3f4815be9b77f4f09c66f
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-26 17:06:06 -0500

    More backend setup
    Connected app to mongodb using mongoose. Made
    env variables to hide authentication info.
    Indicated type of dependencies in package.json

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+const COLLECTION = 'PASS';
 
-app.listen(5000);
+mongoose
+    .connect(`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.e9noz.mongodb.net/${COLLECTION}?retryWrites=true&w=majority&appName=Cluster0`)
+    .then(() => {
+        app.listen(5000);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
