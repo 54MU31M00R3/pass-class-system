43d156e823a4e9d78e4e390c55813b50714aafab
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-25 19:00:20 -0500

    Add functionality to WorksheetForm
    Inputs added to accept files and select to switch
    to announcement form

--- frontend/src/content/components/WorksheetForm.jsx
+++ frontend/src/content/components/WorksheetForm.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
-function WorksheetForm() {
+function WorksheetForm({ formToggler }) {
+    const contentModeHandler = (event) => {
+        event.preventDefault();
 
+        const mode = event.target.value;
+        formToggler(mode);
+    }
+
+    const worksheetSubmit = (event) => {
+        event.preventDefault();
+    }
     return (
-        <div>WorksheetForm</div>
+        <>
+            <div className='formContainer'>
+                <div className='annForm'>
+                    <form className='formDetails'>
+                        <label htmlFor='title'>Title</label>
+                        <input id='title' type='text' />
+                        <label htmlFor='worksheet'>Worksheet</label>
+                        <input id='worksheet' type='file' />
+                        <button onClick={worksheetSubmit}>Upload</button>
+                    </form>
+                    <label htmlFor='selectedmode'>Select Upload Type</label>
+                    <select name="selectedmode" onChange={contentModeHandler} defaultValue="Worksheet">
+                        <option disabled>Worksheet</option>
+                        <option value="announcement">Announcement</option>
+                    </select>
+                </div>
+            </div>
+        </>
     )
 }
 
