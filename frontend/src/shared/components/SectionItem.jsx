0628b6da58181fabebb2f2e3db6e6798271a65b2
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-25 22:37:43 -0500

    Add redirect to login before viewing sections

--- frontend/src/shared/components/SectionItem.jsx
+++ frontend/src/shared/components/SectionItem.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { AuthContext } from '../context/auth-context';
 
 import { Link } from 'react-router-dom';
 
 function SectionItem({ id, courseName, courseSection, timeOfSession, buildingRoomNumber }) {
+    const auth = useContext(AuthContext);
+
     return (
         <ul className='sectionInfo'>
             <div className="upperInfo">
@@ -14,7 +18,7 @@ function SectionItem({ id, courseName, courseSection, timeOfSession, buildingRoo
                     <li>Time: {timeOfSession}</li>
                     <li>Location: {buildingRoomNumber}</li>
                 </div>
-                <Link className='sectionLink' to={`/${id}/section`}>VIEW</Link>
+                <Link className='sectionLink' to={auth.isLoggedIn && `/${id}/section` || !auth.isLoggedIn && '/login'}>VIEW</Link>
             </div>
         </ul>
     )
