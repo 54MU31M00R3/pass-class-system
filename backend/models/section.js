cdd8e7bc520461cdb6a1df2fa45b9e474c873735
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-26 18:05:56 -0500

    Add schema for pass section using mongoose

---
+++ backend/models/section.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const sectionSchema = new Schema({
+    courseName: { type: String, required: true },
+    courseSection: { type: String, required: true },
+    timeOfSession: { type: String, required: true},
+    buildingRoomNumber: { type: String, required: true},
+    students: [{type: mongoose.Types.ObjectId, required: true, ref: 'User'}],
+    mentor: {type: mongoose.Types.ObjectId, required: true, ref: 'User'}
+})
+
+export default mongoose.model('Section', sectionSchema);
--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 5 },
-    role: { type: String, required: true },
+    role: { type: String, required: true, enum: ['student', 'pass leader', 'supervisor'] },
     yuId: { type: String, required: true, minlength: 9, unique: true},
     sections: [{type: mongoose.Types.ObjectId, required: true, ref: 'Section'}]
 })
