d166a0636e81b01619c0a1b2bb844629ed7518d4
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-25 22:02:50 -0500

    Add dummy login functionality

--- frontend/src/auth/components/LoginForm.jsx
+++ frontend/src/auth/components/LoginForm.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { AuthContext } from '../../shared/context/auth-context';
+
+import users from '../../assets/dummyData/users.json';
 
 function LoginForm({ formToggler }) {
+    const auth = useContext(AuthContext);
+    const navigate = useNavigate();
+
     const loginSubmit = (event) => {
         event.preventDefault();
+
+        const foundUser = users.find(user => (user.email === email.value));
+        if (foundUser && (foundUser.password === password.value)){
+            auth.login(foundUser.id, foundUser.role);
+            navigate('/');
+        } else {
+            email.value = '';
+            password.value = '';
+        }
     }
 
     return (
@@ -11,9 +28,9 @@ function LoginForm({ formToggler }) {
                 <div className='authForm'>
                     <form className='formDetails'>
                         <label htmlFor='email'>Email</label>
-                        <input id='email' type='text' />
+                        <input id='email' type='email' />
                         <label htmlFor='password'>Password</label>
-                        <input id='password' type='text' />
+                        <input id='password' type='password' />
                         <button onClick={loginSubmit}>LOGIN</button>
                     </form>
                     <button onClick={formToggler}>SWITCH TO SIGNUP</button>
