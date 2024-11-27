5acbdf7f329edab5682b9c82e8c1a2445314e34b
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-26 20:05:02 -0500

    Add user routes middleware to app

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
+import userRouter from './routes/users-routes';
+
 const app = express();
 
 const USERNAME = process.env.USERNAME;
@@ -9,6 +11,8 @@ const PASSWORD = process.env.PASSWORD;
 const COLLECTION = 'PASS';
 
 app.use(bodyParser.json());
+
+app.use('/api/users', userRouter);
 
 mongoose
     .connect(`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.e9noz.mongodb.net/${COLLECTION}?retryWrites=true&w=majority&appName=Cluster0`)
