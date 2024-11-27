de55eeb7729574cb9fc2a0e58e9c297df9468cae
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-26 19:23:51 -0500

    Add body-parser middleware to app

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
+
 const USERNAME = process.env.USERNAME;
 const PASSWORD = process.env.PASSWORD;
 const COLLECTION = 'PASS';
+
+app.use(bodyParser.json());
 
 mongoose
     .connect(`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.e9noz.mongodb.net/${COLLECTION}?retryWrites=true&w=majority&appName=Cluster0`)
