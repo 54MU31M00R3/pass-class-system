562799e7c7782920a56a7ca49862000b00cd170e
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2024-11-26 20:51:25 -0500

    Add mentor field to content to track uploaders

--- backend/models/content.js
+++ backend/models/content.js
@@ -6,7 +6,8 @@ const contentSchema = new Schema({
     section: { type: mongoose.Types.ObjectId, required: true, ref: 'Section' },
     contentType: { type: String, required: true, enum: ['announcement', 'worksheet'] },
     title: { type: String, required: true },
-    datePosted: { type: Date, required: true }
+    datePosted: { type: Date, required: true },
+    mentor: { type: mongoose.Types.ObjectId, required: true, ref: 'User' }
 })
 
 export default mongoose.model('Content', contentSchema);
